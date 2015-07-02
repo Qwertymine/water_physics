00c777eeae1b37db8ce8173619b50d80065a3419
qwertymine3 <qwertymine3@googlemail.com>
2015-07-02 10:26:28 +0100

    Fixed info tools

--- boat_test/infotools.lua
+++ boat_test/infotools.lua
@@ -8,28 +8,10 @@ minetest.register_craftitem("boat_test:infostick", {
 })
 
 minetest.register_craftitem("boat_test:quick_flowstick", {
-	description = "Quick Flow Stick",
+	description = "Flow Stick",
 	inventory_image = "farming_tool_mesehoe.png",
 	on_place = function(itemstack, placer, pointed_thing)
-		minetest.chat_send_all(quick_water_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).x .. " , " .. quick_water_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).z)
-	end,
-	on_use = function(itemstack, user, pointed_thing)
-		itemstack:replace("boat_test:flowstick")
-		return itemstack
-	end,
-})
-
-minetest.register_craftitem("boat_test:flowstick", {
-	description = "Flow Stick",
-	inventory_image = "farming_tool_diamondhoe.png",
-	on_place = function(itemstack, placer, pointed_thing)
-		if minetest.get_item_group(minetest.get_node(pointed_thing.above).name, "liquid") ~= 0 then
-			minetest.chat_send_all(water_flow(pointed_thing.above).x .. "," .. water_flow(pointed_thing.above).z)
-		end
-	end,
-	on_use = function(itemstack, user, pointed_thing)
-		itemstack:replace("boat_test:quick_flowstick")
-		return itemstack
+		minetest.chat_send_all(quick_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).x .. " , " .. quick_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).z)
 	end,
 })
 
