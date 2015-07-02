68d14467f5f1e6d7cca73a30281ee9f7afa34561
qwertymine3 <qwertymine3@googlemail.com>
2015-07-02 10:28:20 +0100

    Fixed crafting

--- boat_test/infotools.lua
+++ boat_test/infotools.lua
@@ -9,14 +9,14 @@ minetest.register_craftitem("boat_test:infostick", {
 
 minetest.register_craftitem("boat_test:quick_flowstick", {
 	description = "Flow Stick",
-	inventory_image = "farming_tool_mesehoe.png",
+	inventory_image = "farming_tool_diamondhoe.png",
 	on_place = function(itemstack, placer, pointed_thing)
 		minetest.chat_send_all(quick_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).x .. " , " .. quick_flow(pointed_thing.above,minetest.get_node(pointed_thing.above)).z)
 	end,
 })
 
 minetest.register_craft({
-	output = "boat_test:flowstick",
+	output = "boat_test:quick_flowstick",
 	recipe = {
 		{"default:diamond","default:diamond","default:diamond"},
 		{"default:diamond","default:stick",  "default:diamond"},
